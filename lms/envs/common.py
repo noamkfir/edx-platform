c42ce13307e32c40583d98fc8fdcd0ec6d604fa3
Jason Bau <jbau@stanford.edu>
2013-06-03 17:48:44 -0700

    Initial commit for enabling shibboleth logins using external_auth

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -88,6 +88,7 @@ MITX_FEATURES = {
     'AUTH_USE_OPENID': False,
     'AUTH_USE_MIT_CERTIFICATES': False,
     'AUTH_USE_OPENID_PROVIDER': False,
+    'AUTH_USE_SHIB': False,
 
     # analytics experiments
     'ENABLE_INSTRUCTOR_ANALYTICS': False,
